d709a3f7a9b112753d608e70d02538d588398d40
faruukh-ccproject <faruukh.ccproject@gmail.com>
2020-09-02 14:24:10 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./style.css";
+import {Button} from "reactstrap"
 
 export default function App() {
+
+const addOrganizationERP = (e) =>{
+    alert('click');
+}
+
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+       <Button.Ripple outline color="success" onClick={addOrganizationERP} >
+            
+                Add More
+              </Button.Ripple>
     </div>
   );
 }
